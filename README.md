327210036f213c8250800e3c92973af271c86adf
Manji Gupta <manjitkumar7667693313@gmail.com>
2025-09-11 10:19:00 +0530

    update folder

--- README.md
+++
@@ -1,4 +0,0 @@
-# apnacollege-demo
-This is my first Git Repository.
-<br>
-Author - manjit kumar
--- first.js
+++ first.js
@@ -1,3 +1,11 @@
+
+// simple JavaScript program that:
+// Declares two variables (x and y) using let.
+// Prints their sum.
+// Prints a text message.
+// Prints their multiplication result.
+
+
 // let x = 5;
 // let y = 10;
 
@@ -5,16 +13,39 @@
 // console.log("first JavaScript");
 // console.log("multiple of x,y",x*y);
 
+
+// -----------------------------------------
+
+
+
+
 // const price1=5;
 // const price2=6;
 // let total =price1 +price2;
 // console.log("sum",total);
 
+// ---------------------------------------------------------
+// let name = "Manjit Kumar";
+// let midName = "Gupta";
+// let fullName = name + " " + midName;
 
-let name = "Manjit Kumar";
-let midName = "Gupta";
-let fullName = name + " " + midName;
+// console.log("Full name:", fullName);
 
-console.log("Full name:", fullName);
+// -------------------------------------------------------
+// {var x = 5;
+// var y = 10;}
+
+// console.log("sum of x and y:", x + y);
+// console.log("first JavaScript");
+// console.log("multiple of x,y",x*y);
+
+// -------------------------------------------------------
 
 
+function test() {
+  if (true) {
+    var x = 10;
+  }
+  console.log(x); // 10 (because var is function-scoped)
+}
+test();
