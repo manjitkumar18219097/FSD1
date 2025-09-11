e2ae0c3f7712a0ec3f1bb1f689450c1538b19837
Manji Gupta <manjitkumar7667693313@gmail.com>
2025-09-11 09:51:58 +0530

    Your commit message

---
+++ first.js
@@ -0,0 +1,20 @@
+// let x = 5;
+// let y = 10;
+
+// console.log("sum of x and y:", x + y);
+// console.log("first JavaScript");
+// console.log("multiple of x,y",x*y);
+
+// const price1=5;
+// const price2=6;
+// let total =price1 +price2;
+// console.log("sum",total);
+
+
+let name = "Manjit Kumar";
+let midName = "Gupta";
+let fullName = name + " " + midName;
+
+console.log("Full name:", fullName);
+
+
